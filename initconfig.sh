f588053be1411eec1e08ee55141073c8834f6159
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2014-08-04 18:11:27 +0100

    doc tweak

--- initconfig.sh
+++ initconfig.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+#
+# Copy the skeletal files from the master source checkout.
+#
 echo "copying initial configuration to /etc/shinken"
 cp -r /home/shinken/shinken-master/etc/* /etc/shinken
